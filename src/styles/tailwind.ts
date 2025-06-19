0b74c7f40e6372df4c23a2e1e266624370181da4
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-19 19:21:43 +0400

    create heading styles in tailwind.ts

---
+++ src/styles/tailwind.ts
@@ -0,0 +1,15 @@
+
+export default function tailwind() {
+    const H1 = "text-[5.6rem] font-[700] leading-[56px] tracking-[2px] text-[rgba(0,0,0,1)]"
+    const H2 = "text-[4rem] font-[700] leading-[44px] tracking-[1.43px] text-[rgba(0,0,0,1)]"
+    const H3 = "text-[3.2rem] font-[700] leading-[36px] tracking-[1.14px] text-[rgba(0,0,0,1)]"
+    const H4 = "text-[2.8rem] font-[700] leading-[100%] tracking-[2px] text-[rgba(0,0,0,1)]"
+    const H5 = "text-[2.4rem] font-[700] leading-[100%] tracking-[1.71px] text-[rgba(0,0,0,1)]"
+    const H6 = "text-[1.8rem] font-[700] leading-[100%] tracking-[1.29px] text-[rgba(0,0,0,1)]"
+    const Overline = "text-[1.4rem] font-[400] leading-[100%] tracking-[10px] text-[rgba(216,125,74,1)]"
+    const SubTitle = "text-[1.3rem] font-[700] leading-[25px] tracking-[0.93px] text-[rgba(216,125,74,1)]"
+    const P = "text-[1.5rem] font-[400] leading-[25px] tracking-[0] text-[rgba(0,0,0,1)]"
+
+
+    return { H1, H2, H3, H4, H5, H6, Overline, SubTitle, P }
+}
